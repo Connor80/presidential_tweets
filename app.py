9e755ced0428ee723b192b1fccfbccf4842a3a69
Connor80 <connor@connortroy.com>
2018-04-14 18:52:33 -0500

    retrieve and display tweets

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
 import datetime
+from flask_tweepy import Tweepy
+import tweepy
 
 app = Flask(__name__)
+
+app.config.from_object('config')
+auth = tweepy.OAuthHandler(app.config['CONSUMERKEY'], app.config['CONSUMERSECRET'])
+auth.set_access_token(app.config['ACCESSTOKENKEY'], app.config['ACCESSTOKENSECRET'])
+tweepy = tweepy.API(auth)
 
 @app.route('/')
 def index():
@@ -9,7 +16,19 @@ def index():
     today = datetime.date.today()
     diff = today - start
     day = diff.days
-    return render_template('layout.html', day=day)
+    username44 = '@BarackObama'
+    username45 = '@realDonaldTrump'
+    return render_template('layout.html', day=day, obamatweets=get_obama_tweets(username44),
+        trumptweets=get_trump_tweets(username45))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def get_obama_tweets(username44):
+    obamatweets = tweepy.user_timeline(username44)
+    return [{'tweet': t.text, 'created_at': t.created_at, 'username': username44,
+              'headshot_url': t.user.profile_image_url}
+           for t in obamatweets]
+
+def get_trump_tweets(username45):
+    trumptweets = tweepy.user_timeline(username45)
+    return [{'tweet': t.text, 'created_at': t.created_at, 'username': username45,
+          'headshot_url': t.user.profile_image_url}
+       for t in trumptweets]
