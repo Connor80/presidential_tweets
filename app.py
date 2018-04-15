240529b12cc889905fba8815d506538135f94ac2
Connor80 <connor@connortroy.com>
2018-04-14 22:35:23 -0500

    datetime for obama

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template
 import datetime
-import pytz
-import sys
 import tweepy
 
 app = Flask(__name__)
@@ -11,29 +9,31 @@ auth = tweepy.OAuthHandler(app.config['CONSUMERKEY'], app.config['CONSUMERSECRET
 auth.set_access_token(app.config['ACCESSTOKENKEY'], app.config['ACCESSTOKENSECRET'])
 tweepy = tweepy.API(auth)
 
-start = datetime.date(2017, 1, 20)
+inauguration44 = datetime.date(2009, 1, 20)
+inauguration45 = datetime.date(2017, 1, 20)
 today = datetime.date.today()
-diff = today - start
+diff = today - inauguration45
 day = diff.days
+date = inauguration44 + datetime.timedelta(days=day)
 
 @app.route('/')
 def index():
     today = str(datetime.date.today())
     username44 = '@BarackObama'
     username45 = '@realDonaldTrump'
-    return render_template('layout.html', day=day, obamatweets=get_obama_tweets(username44),
-        trumptweets=get_trump_tweets(username45), today=today)
+    return render_template('layout.html', day=day, date=date, today=today,
+        obamatweets=get_obama_tweets(username44), trumptweets=get_trump_tweets(username45))
 
 def get_obama_tweets(username44):
     obamatweets = tweepy.user_timeline(username44)
-    start = datetime.date(2009, 1, 20)
-    date = start + datetime.timedelta(days=day)
-    return [{'tweet': t.text, 'created_at': t.created_at, 'username': username44,
+    return [{'tweet': t.text, 'created_at': t.created_at.replace().strftime('%Y-%m-%d'),
+              'username': username44,
               'headshot_url': t.user.profile_image_url}
            for t in obamatweets]
 
 def get_trump_tweets(username45):
     trumptweets = tweepy.user_timeline(username45)
-    return [{'tweet': t.text, 'created_at': t.created_at.replace().strftime('%Y-%m-%d'), 'username': username45,
+    return [{'tweet': t.text, 'created_at': t.created_at.replace().strftime('%Y-%m-%d'),
+              'username': username45,
               'headshot_url': t.user.profile_image_url}
            for t in trumptweets]
